f610aa3b51c38dbc1d95226aae8918a5f7718064
Danil Lukonin <OldBlackTree@gmail.com>
2019-04-28 13:44:39 +0300

    feat: add paint tool logic

--- palette/index.js
+++ palette/index.js
@@ -6,19 +6,30 @@ const colorPickerEl = document.getElementById('color-picker');
 const moveEl = document.getElementById('move');
 const transformEl = document.getElementById('transform');
 
-let currentColor = '';
+let currentColor = 'green';
 let prevColor = '';
 
 
 
-document.addEventListener('click', chooseColor)
-function chooseColor(e){
-  prevColor= currentColor;
-  currentColor = getComputedStyle(e.target).backgroundColor;
-  if (currentColor !== prevColor ) {
-    document.getElementById('prev-color').style.backgroundColor = prevColor;
+
+
+document.addEventListener('click', paint);
+function paint(e){
+  if (e.target.classList.contains('palette__item')){
+   e.target.style.backgroundColor = currentColor;
   }
-  document.getElementById('current-color').style.backgroundColor = currentColor;
-  console.log(currentColor);
 }
+
+
+
+// document.addEventListener('click', chooseColor)
+// function chooseColor(e){
+//   prevColor= currentColor;
+//   currentColor = getComputedStyle(e.target).backgroundColor;
+//   if (currentColor !== prevColor ) {
+//     document.getElementById('prev-color').style.backgroundColor = prevColor;
+//   }
+//   document.getElementById('current-color').style.backgroundColor = currentColor;
+//   console.log(currentColor);
+// }
 
